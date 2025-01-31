727fdf94efc803a0d8dfe77497371aabc0d1b319
Nikita Sigarev <nikita_sigarev@mail.ru>
2025-01-31 12:31:48 +0700

    prettier + esling

---
+++ src/components/heHead.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="header">
+    <a class="title">Жилфонд</a>
+    <a class="text">Пользователь</a>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 100%;
+}
+.title {
+  font-weight: 700;
+  font-size: 32px;
+}
+.text {
+  font-size: 16px;
+}
+</style>
---
+++ src/components/mainComponent.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import MyHead from "./heHead.vue";
+</script>
+
+<template>
+  <div class="main">
+    <my-head></my-head>
+    <my-body></my-body>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  width: 100%;
+  height: 100%;
+}
+</style>

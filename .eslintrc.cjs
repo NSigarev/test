d631c8c6e12552027e12d2edb2780e1ca4afd8c5
Nikita Sigarev <nikita_sigarev@mail.ru>
2025-01-31 11:23:19 +0700

    prettier + esling

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,20 @@
 module.exports = {
   extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier',
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier",
   ],
   rules: {
-    'vue/no-deprecated-v-bind-sync': 'off',
-    'vue/no-deprecated-v-on-native-modifier': 'off',
-    'vue/no-deprecated-destroyed-lifecycle': 'off',
-    'vue/no-v-for-template-key-on-child': 'off',
-    'vue/no-deprecated-slot-attribute': 'off',
-    'vue/no-mutating-props': 'off',
+    "vue/no-deprecated-v-bind-sync": "off",
+    "vue/no-deprecated-v-on-native-modifier": "off",
+    "vue/no-deprecated-destroyed-lifecycle": "off",
+    "vue/no-v-for-template-key-on-child": "off",
+    "vue/no-deprecated-slot-attribute": "off",
+    "vue/no-mutating-props": "off",
   },
   root: true,
-  'env': {
-    'vue/setup-compiler-macros': true,
+  env: {
+    "vue/setup-compiler-macros": true,
   },
 };
